e47f615f72208a4631af995eca55ebd049c0309a
Itsnotf <faizaflah009@gmail.com>
2024-02-10 19:07:09 +0700

    first commit

---
+++ src/components/navbar/index.tsx
@@ -0,0 +1,55 @@
+import Image from "next/image";
+import hambuger from "../../../public/assets/hamburger.svg";
+import logo from "../../../public/assets/logo.svg";
+import Link from "next/link";
+
+export default function Navbar() {
+  interface NavMenuItem {
+    name: string;
+  }
+
+  const navMenu: NavMenuItem[] = [
+    {
+      name: "Home",
+    },
+    {
+      name: "Discover",
+    },
+    {
+      name: "Special Deals",
+    },
+    {
+      name: "Contact",
+    },
+  ];
+
+  return (
+    <div className="mx-4 py-[38px] flex justify-between items-center md:mx-16 lg:mx-32 font-bold  ">
+      <div className="flex gap-2 justify-center items-center lg:order-1 sm:order-3">
+        <Image src={logo} alt="Logo Website" />
+        <h1 className="text-2xl">Travlog</h1>
+      </div>
+
+      <div className="hidden lg:flex lg:order-2 ">
+        <ul className="gap-16 flex text-sm">
+          {navMenu.map((item, index) => (
+            <li key={index} className="text-[#19182580] hover:text-hitam ">
+              <Link href={`#${item.name}`}>{item.name}</Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="text-sm hidden sm:flex lg:order-3 sm:order-1">
+        <button className=" py-4 px-8">Log In</button>
+        <button className="py-4 px-8 rounded-full bg-ungu text-white">
+          Sign Up
+        </button>
+      </div>
+
+      <div className="lg:hidden order-4">
+        <Image src={hambuger} alt="Hamburger Menu" />
+      </div>
+    </div>
+  );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,8 +13,31 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        ungu: "#5D50C6",
+        hitam: "#222831",
+      },
+      boxShadow: {
+        "white-shadow": [
+          "0px 0px 0px 0px rgba(0, 0, 0, 0.07)",
+          "0px 34px 75px 0px rgba(0, 0, 0, 0.07)",
+          "0px 137px 137px 0px rgba(0, 0, 0, 0.06)",
+          "0px 308px 185px 0px rgba(0, 0, 0, 0.04)",
+          "0px 548px 219px 0px rgba(0, 0, 0, 0.01)",
+          "0px 856px 240px 0px rgba(0, 0, 0, 0.00)",
+        ].join(", "),
+        "black-shadow": [
+          "0px 0px 0px 0px rgba(0, 0, 0, 0.10)",
+          "0px 5px 11px 0px rgba(0, 0, 0, 0.10)",
+          "0px 20px 20px 0px rgba(0, 0, 0, 0.09)",
+          "0px 45px 27px 0px rgba(0, 0, 0, 0.05)",
+          "0px 81px 32px 0px rgba(0, 0, 0, 0.01)",
+          "0px 126px 35px 0px rgba(0, 0, 0, 0.00)",
+        ].join(", "),
+      },
     },
   },
   plugins: [],
 };
+
 export default config;
